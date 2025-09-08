8816bd5e82a965e8e060bdfef1f2eb1422bdbbda
Federico Lazarte <fedelazarte2015@gmail.com>
2025-09-08 20:27:55 -0300

    add object Math

---
+++ 17-Objeto Math/app.js
@@ -0,0 +1,12 @@
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.abs(-7.8)); // 7.8
+console.log(Math.ceil(7.8)); // redondea al número entero mayor
+console.log(Math.floor(7.8)); // redondea al número entero menor
+console.log(Math.round(7.2)); // redondea al número entero más cercano
+console.log(Math.sqrt(2)); // raíz cuadrada
+console.log(Math.pow(2, 5)); // 32, 2 elevado al 5
+console.log(Math.sign(-7.8)); // si es neg -1, si es 0 da 0, y si es pos devuelve 1
+console.log(Math.random());
+
+console.log(Math.round(Math.random() * 1000)); // entre 0 y 1000
