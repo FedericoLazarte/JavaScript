8628f44577860cba149292dd2919e31bc457f397
Federico Lazarte <fedelazarte2015@gmail.com>
2025-09-09 16:08:10 -0300

    add modules in JS

---
+++ 19-Módulos/js/constantes.js
@@ -0,0 +1,17 @@
+export const PI = Math.PI;
+
+export let usuario = "Jon";
+
+const password = "qwerty";
+
+const hello = () => console.log("Hola");
+
+export default function saludar() {
+	console.log("Hola móodulos +ES6");
+}
+
+export class Saludar {
+	constructor() {
+		console.log("Hola Clases +ES6");
+	}
+}
